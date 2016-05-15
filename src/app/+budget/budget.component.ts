20e2dda7cc479f48be106c2550ee705eb56345db
Harshit Purwar <harshit.purwar@hc14-2.local>
2016-05-15 15:54:15 +0530

    Adding UI items

--- src/app/+budget/budget.component.ts
+++ src/app/+budget/budget.component.ts
@@ -3,12 +3,14 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../account/account.service';
 import { Account } from '../account/account';
 import { BudgetService } from '../budget/budget.service';
+import { BadgeListComponent } from '../badge-list/badge-list.component';
  
 @Component({
   moduleId: module.id,
   selector: 'app-budget',
   templateUrl: 'budget.component.html',
-  styleUrls: ['budget.component.css']
+  styleUrls: ['budget.component.css'],
+  directives: [BadgeListComponent]
 })
 export class BudgetComponent implements OnInit {
 
@@ -16,7 +18,7 @@ export class BudgetComponent implements OnInit {
   model: any = {};
   accounts: Account[];
   ngOnInit() {
-  	this.model.budget = this.budgetService.getBudget();
+  	this.budgetService.getBudget().then(budget => this.model.budget = budget);
   	this.accountService.getExpenseAccount().then(expenseAccount => {
   		this.accountService.getChildrenAccounts(expenseAccount).then(childrenAccounts => {
   			this.accounts = childrenAccounts;
