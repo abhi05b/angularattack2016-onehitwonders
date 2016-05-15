12bb828cf390fdd2f6d0781f5e8eedfd25b3fd29
Abhishek <abhi05b@gmail.com>
2016-05-16 01:57:57 +0530

    Adding table fields parsing

--- src/app/+transactions/+list/list.component.ts
+++ src/app/+transactions/+list/list.component.ts
@@ -50,7 +50,6 @@ export class ListComponent implements OnInit {
   ngOnInit() {
     
     this.transactionService.getTransactions().then(transactions => {
-      console.log('Here ', transactions);
           this.transactions = transactions;
           if (transactions.length)
               this.formatTransactions(transactions);
@@ -63,7 +62,6 @@ export class ListComponent implements OnInit {
     
       this.dataModel = transactions.map(transaction => {
         let t = new TableDataModel(transaction.date, transaction.merchant, transaction.amount, transaction.from.name, transaction.to.name, transaction.comments, transaction.tags);      
-        console.log(t);
         return t;
       });
 
@@ -75,7 +73,6 @@ export class ListComponent implements OnInit {
   }
 
   changePage(page: any, data: Array<any> = this.data): Array<any> {
-    console.log(page);
     let start = (page.page - 1) * page.itemsPerPage;
     let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
     return data.slice(start, end);
